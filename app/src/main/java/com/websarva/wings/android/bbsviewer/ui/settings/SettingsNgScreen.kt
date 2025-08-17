b6eafcaf4ab9ecd78ac3c1722171d19eba998b6c
haruk <harukimatsuo461@gmail.com>
2025-08-17 20:37:59 +0900

    style: SettingsScreenから不要なimportを削除し、SettingsNgScreenにnavigationBarsPaddingを適用

--- app/src/main/java/com/websarva/wings/android/bbsviewer/ui/settings/SettingsNgScreen.kt
+++ app/src/main/java/com/websarva/wings/android/bbsviewer/ui/settings/SettingsNgScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -79,6 +80,7 @@ fun SettingsNgScreen(
         bottomBar = {
             if (uiState.selectMode) {
                 BbsSelectBottomBar(
+                    modifier = Modifier.navigationBarsPadding(),
                     onDelete = { viewModel.removeSelected() },
                     onOpen = {}
                 )
--- app/src/main/java/com/websarva/wings/android/bbsviewer/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/websarva/wings/android/bbsviewer/ui/settings/SettingsScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.websarva.wings.android.bbsviewer.ui.topbar.HomeTopAppBarScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
