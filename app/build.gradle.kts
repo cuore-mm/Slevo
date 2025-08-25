86680f3f3bd7d02cc5c7f43ff2ec8c2d39e56da5
haruk <harukimatsuo461@gmail.com>
2025-08-25 14:34:39 +0900

    Enable minification and resource shrinking for release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,13 +43,18 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+        debug {
+            isMinifyEnabled = false
+        }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
