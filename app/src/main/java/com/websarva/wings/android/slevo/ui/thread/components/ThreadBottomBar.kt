9269ac2220bb9da61bc4f4a1583c9cc26ef39111
cuore <162403608+cuore-mm@users.noreply.github.com>
2025-09-03 12:27:53 +0900

    feat: redesign thread bottom bar

--- app/src/main/java/com/websarva/wings/android/slevo/ui/thread/components/ThreadBottomBar.kt
+++ app/src/main/java/com/websarva/wings/android/slevo/ui/thread/components/ThreadBottomBar.kt
@@ -1,8 +1,11 @@
 package com.websarva.wings.android.slevo.ui.thread.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountTree
 import androidx.compose.material.icons.filled.Create
@@ -10,41 +13,104 @@ import androidx.compose.material.icons.filled.CropSquare
 import androidx.compose.material.icons.filled.FormatListNumbered
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.StarOutline
 import androidx.compose.material3.BottomAppBarScrollBehavior
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FlexibleBottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.websarva.wings.android.slevo.R
+import com.websarva.wings.android.slevo.data.model.ThreadInfo
+import com.websarva.wings.android.slevo.ui.common.bookmark.SingleBookmarkState
+import com.websarva.wings.android.slevo.ui.theme.bookmarkColor
+import com.websarva.wings.android.slevo.ui.thread.state.ThreadUiState
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun ThreadBottomBar(
     modifier: Modifier = Modifier,
+    uiState: ThreadUiState,
     isTreeSort: Boolean,
     onSortClick: () -> Unit,
     onPostClick: () -> Unit,
     onTabListClick: () -> Unit,
     onRefreshClick: () -> Unit,
     onSearchClick: () -> Unit,
+    onBookmarkClick: () -> Unit,
     scrollBehavior: BottomAppBarScrollBehavior? = null,
 ) {
     FlexibleBottomAppBar(
         modifier = modifier,
-        scrollBehavior = scrollBehavior,
-        content = {
+        scrollBehavior = scrollBehavior
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = onBookmarkClick) {
+                    if (uiState.singleBookmarkState.isBookmarked) {
+                        Box {
+                            Icon(
+                                imageVector = Icons.Filled.Star,
+                                contentDescription = null,
+                                tint = uiState.singleBookmarkState.selectedGroup?.colorName?.let {
+                                    bookmarkColor(it)
+                                } ?: LocalContentColor.current
+                            )
+                            Icon(
+                                imageVector = Icons.Outlined.StarOutline,
+                                contentDescription = stringResource(R.string.bookmark)
+                            )
+                        }
+                    } else {
+                        Icon(
+                            imageVector = Icons.Outlined.StarOutline,
+                            contentDescription = stringResource(R.string.bookmark)
+                        )
+                    }
+                }
+                Text(
+                    text = uiState.threadInfo.title,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(horizontal = 8.dp)
+                )
+                IconButton(onClick = onRefreshClick) {
+                    Icon(
+                        imageVector = Icons.Filled.Refresh,
+                        contentDescription = stringResource(R.string.refresh)
+                    )
+                }
+            }
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 IconButton(onClick = onSortClick) {
                     Icon(
-                        if (isTreeSort) Icons.Default.AccountTree else Icons.Default.FormatListNumbered,
+                        if (isTreeSort) Icons.Filled.AccountTree else Icons.Filled.FormatListNumbered,
                         contentDescription = stringResource(
                             if (isTreeSort) R.string.tree_order else R.string.number_order
                         )
@@ -52,32 +118,25 @@ fun ThreadBottomBar(
                 }
                 IconButton(onClick = onSearchClick) {
                     Icon(
-                        Icons.Default.Search,
+                        imageVector = Icons.Filled.Search,
                         contentDescription = stringResource(R.string.search)
                     )
                 }
                 IconButton(onClick = onTabListClick) {
                     Icon(
-                        Icons.Default.CropSquare,
+                        imageVector = Icons.Filled.CropSquare,
                         contentDescription = stringResource(R.string.open_tablist)
-                    )
-                }
-                IconButton(onClick = onRefreshClick) {
-                    Icon(
-                        Icons.Default.Refresh,
-                        contentDescription = stringResource(R.string.refresh)
                     )
                 }
                 IconButton(onClick = onPostClick) {
                     Icon(
-                        Icons.Default.Create,
+                        imageVector = Icons.Filled.Create,
                         contentDescription = stringResource(R.string.post)
                     )
                 }
             }
-
         }
-    )
+    }
 }
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
@@ -85,11 +144,22 @@ fun ThreadBottomBar(
 @Composable
 fun ThreadBottomBarPreview() {
     ThreadBottomBar(
+        uiState = ThreadUiState(
+            threadInfo = ThreadInfo(
+                title = "スレッドのタイトル"
+            ),
+            singleBookmarkState = SingleBookmarkState(
+                isBookmarked = false,
+                selectedGroup = null
+            )
+        ),
         isTreeSort = false,
         onSortClick = {},
         onPostClick = {},
         onTabListClick = {},
         onRefreshClick = {},
         onSearchClick = {},
+        onBookmarkClick = {}
     )
 }
+
