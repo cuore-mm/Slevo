04c9ca2ea69bbc13cb1435bff0b6968f2e37c1dc
cuore <162403608+cuore-mm@users.noreply.github.com>
2025-09-05 10:38:59 +0900

    feat: place cursor at message end

--- app/src/main/java/com/websarva/wings/android/slevo/ui/common/PostDialog.kt
+++ app/src/main/java/com/websarva/wings/android/slevo/ui/common/PostDialog.kt
@@ -25,7 +25,10 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.FocusRequester
@@ -33,7 +36,9 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -135,9 +140,20 @@ fun PostDialog(
                                 .padding(8.dp)
                         )
                     }
+                    var textFieldValue by remember(message) {
+                        mutableStateOf(
+                            TextFieldValue(
+                                text = message,
+                                selection = TextRange(message.length)
+                            )
+                        )
+                    }
                     OutlinedTextField(
-                        value = message,
-                        onValueChange = { onMessageChange(it) },
+                        value = textFieldValue,
+                        onValueChange = {
+                            textFieldValue = it
+                            onMessageChange(it.text)
+                        },
                         placeholder = { Text(stringResource(R.string.post_message)) },
                         modifier = Modifier
                             .fillMaxWidth()
