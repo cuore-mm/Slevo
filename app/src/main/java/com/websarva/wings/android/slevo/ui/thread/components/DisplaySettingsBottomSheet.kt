f8113b33fe14c1c9c05c16004c615e2213e52b14
haruk <harukimatsuo461@gmail.com>
2025-09-16 22:09:41 +0900

    fix: 表示設定のUIを改善
    表示設定の個別設定項目をカードで囲み、スライダーのラベルに太字を適用しました。
    また、行間の設定値を%表示に変更し、スライダーの溝の色を調整しました。
    文言の修正も行っています。

--- app/src/main/java/com/websarva/wings/android/slevo/ui/thread/components/DisplaySettingsBottomSheet.kt
+++ app/src/main/java/com/websarva/wings/android/slevo/ui/thread/components/DisplaySettingsBottomSheet.kt
@@ -7,10 +7,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderColors
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -21,7 +25,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -93,7 +96,6 @@ fun DisplaySettingsContent(
             snapFactor = 20,
             steps = 0,
             enabled = !isIndividual,
-            valueTextStyle = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
             valueFormatter = { v ->
                 String.format(
                     Locale.getDefault(),
@@ -105,56 +107,77 @@ fun DisplaySettingsContent(
         )
 
         Spacer(modifier = Modifier.height(8.dp))
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(text = stringResource(R.string.individual_text_settings))
-            Spacer(modifier = Modifier.weight(1f))
-            Switch(checked = isIndividual, onCheckedChange = onIndividualChange)
-        }
-        if (isIndividual) {
-            Spacer(modifier = Modifier.height(8.dp))
-            LabeledSlider(
-                labelRes = R.string.header_text_size,
-                value = headerTextScale,
-                valueRange = 0.7f..1.6f,
-                snapFactor = 20,
-                steps = 0,
-                valueFormatter = { v ->
-                    String.format(
-                        Locale.getDefault(),
-                        "%d%%",
-                        (v * 100).roundToInt()
+        Card (
+            modifier = Modifier.fillMaxWidth(),
+            shape = MaterialTheme.shapes.large,
+            elevation = CardDefaults.cardElevation()
+        ){
+            Column(modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = stringResource(R.string.individual_text_settings),
                     )
-                },
-                onValueChange = onHeaderTextScaleChange
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            LabeledSlider(
-                labelRes = R.string.body_text_size,
-                value = bodyTextScale,
-                valueRange = 0.7f..1.6f,
-                snapFactor = 20,
-                steps = 0,
-                valueFormatter = { v ->
-                    String.format(
-                        Locale.getDefault(),
-                        "%d%%",
-                        (v * 100).roundToInt()
+                    Spacer(modifier = Modifier.weight(1f))
+                    Switch(checked = isIndividual, onCheckedChange = onIndividualChange)
+                }
+                if (isIndividual) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    LabeledSlider(
+                        labelRes = R.string.header_text_size,
+                        value = headerTextScale,
+                        valueRange = 0.7f..1.6f,
+                        snapFactor = 20,
+                        steps = 0,
+                        valueFormatter = { v ->
+                            String.format(
+                                Locale.getDefault(),
+                                "%d%%",
+                                (v * 100).roundToInt()
+                            )
+                        },
+                        onValueChange = onHeaderTextScaleChange,
+                        sliderColors = SliderDefaults.colors(
+                            inactiveTrackColor = MaterialTheme.colorScheme.primaryContainer,
+                        )
                     )
-                },
-                onValueChange = onBodyTextScaleChange
-            )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    LabeledSlider(
+                        labelRes = R.string.body_text_size,
+                        value = bodyTextScale,
+                        valueRange = 0.7f..1.6f,
+                        snapFactor = 20,
+                        steps = 0,
+                        valueFormatter = { v ->
+                            String.format(
+                                Locale.getDefault(),
+                                "%d%%",
+                                (v * 100).roundToInt()
+                            )
+                        },
+                        onValueChange = onBodyTextScaleChange,
+                        sliderColors = SliderDefaults.colors(
+                            inactiveTrackColor = MaterialTheme.colorScheme.primaryContainer,
+                        )
+                    )
 
-            Spacer(modifier = Modifier.height(8.dp))
-            LabeledSlider(
-                labelRes = R.string.line_spacing,
-                value = lineHeight,
-                valueRange = 1.2f..1.8f,
-                snapFactor = 10,
-                steps = 7,
-                valueFormatter = { v -> String.format(Locale.getDefault(), "%.1fem", v) },
-                onValueChange = onLineHeightChange
-            )
-            Spacer(modifier = Modifier.height(8.dp))
+                    Spacer(modifier = Modifier.height(16.dp))
+                    LabeledSlider(
+                        labelRes = R.string.line_spacing,
+                        value = lineHeight,
+                        valueRange = 1.2f..1.8f,
+                        snapFactor = 10,
+                        steps = 5,
+                        valueFormatter = { v ->
+                            String.format(Locale.getDefault(), "%d%%", (v * 100).roundToInt())
+                        },
+                        onValueChange = onLineHeightChange,
+                        sliderColors = SliderDefaults.colors(
+                            inactiveTrackColor = MaterialTheme.colorScheme.primaryContainer,
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+            }
         }
     }
 }
@@ -162,35 +185,44 @@ fun DisplaySettingsContent(
 
 @Composable
 fun LabeledSlider(
+    modifier: Modifier = Modifier,
     labelRes: Int,
     value: Float,
     valueRange: ClosedFloatingPointRange<Float>,
     snapFactor: Int = 0,
     steps: Int = 0,
     enabled: Boolean = true,
-    valueTextStyle: TextStyle = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
     valueFormatter: (Float) -> String = { v -> String.format(Locale.getDefault(), "%.2f", v) },
+    sliderColors: SliderColors = SliderDefaults.colors(),
     onValueChange: (Float) -> Unit
 ) {
-    Text(text = stringResource(labelRes))
-    Text(
-        text = valueFormatter(value),
-        style = valueTextStyle,
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentWidth(Alignment.CenterHorizontally)
-    )
-    Slider(
-        value = value,
-        onValueChange = {
-            val newValue =
-                if (snapFactor > 0) (it * snapFactor).roundToInt() / snapFactor.toFloat() else it
-            onValueChange(newValue)
-        },
-        valueRange = valueRange,
-        steps = steps,
-        enabled = enabled
-    )
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(labelRes),
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = valueFormatter(value),
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally),
+        )
+        Slider(
+            value = value,
+            onValueChange = {
+                val newValue =
+                    if (snapFactor > 0) (it * snapFactor).roundToInt() / snapFactor.toFloat() else it
+                onValueChange(newValue)
+            },
+            valueRange = valueRange,
+            steps = steps,
+            enabled = enabled,
+            colors = sliderColors
+        )
+    }
 }
 
 @Preview(showBackground = true)
