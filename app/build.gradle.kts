2de6f5f5e680590f77ace49510bcdc027b7e1153
haruk <harukimatsuo461@gmail.com>
2025-09-17 18:20:52 +0900

    chore: アプリバージョンを1.2.0に更新
    アプリの`versionCode`を6から7に、`versionName`を"1.1.3"から"1.2.0"に更新しました。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.websarva.wings.android.slevo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.1.3"
+        versionCode = 7
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
