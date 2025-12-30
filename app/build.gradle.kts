21ac0daecb28aa34dbda18f455dd0565666b398f
haruk <harukimatsuo461@gmail.com>
2025-12-30 22:54:58 +0900

    chore: アプリケーションのバージョンを1.4.0に更新
    アプリケーションのバージョン情報を更新しました。
    
    - `versionCode`を10から11にインクリメントしました。
    - `versionName`を1.3.1から1.4.0に変更しました。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.websarva.wings.android.slevo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.3.1"
+        versionCode = 11
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
