8ac0f5529db33afee5de3cbaa57325bf1777d85e
cuore <harukimatsuo461@gmail.com>
2026-01-14 06:58:31 +0000

    fix(viewer): サムネイル中央寄せのスクロール処理を修正

--- app/src/main/java/com/websarva/wings/android/slevo/ui/viewer/ImageViewerScreen.kt
+++ app/src/main/java/com/websarva/wings/android/slevo/ui/viewer/ImageViewerScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
