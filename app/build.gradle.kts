d6d8f9a6875cd72e03067679c0b60e7b64aba4ab
cuore <harukimatsuo461@gmail.com>
2026-02-16 17:00:30 +0000

    fix: Kotlin serializationプラグイン版数を2.2.10系へ統一

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("com.google.dagger.hilt.android")
 
     // Kotlin serialization plugin for type safe routes and navigation arguments
-    kotlin("plugin.serialization") version "2.1.0"
+    alias(libs.plugins.kotlin.serialization)
 
     id("com.google.devtools.ksp")
 
