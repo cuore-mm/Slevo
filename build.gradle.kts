8d7c7930c6aaad59032b4e8b673b3ad002ea5f93
cuore <harukimatsuo461@gmail.com>
2026-02-16 01:45:52 +0000

    build: HiltをAGP9対応版へ更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
 
     //hilt
-    id("com.google.dagger.hilt.android") version "2.57.2" apply false
+    id("com.google.dagger.hilt.android") version "2.59.1" apply false
 
     id("com.google.devtools.ksp") version "2.3.2" apply false
 
