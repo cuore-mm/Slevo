7030d0cf98e08aa6084c980f7ed8cad84b76cc2c
cuore <harukimatsuo461@gmail.com>
2026-06-18 09:13:29 +0000

    fix(test): verify import漏れを修正

--- app/src/test/java/com/websarva/wings/android/slevo/ui/thread/viewmodel/ThreadTabCoordinatorTest.kt
+++ app/src/test/java/com/websarva/wings/android/slevo/ui/thread/viewmodel/ThreadTabCoordinatorTest.kt
@@ -10,6 +10,7 @@ import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.slot
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.advanceUntilIdle
